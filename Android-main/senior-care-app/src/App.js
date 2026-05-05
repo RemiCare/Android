4684d92b5c4f3a6b89b435f276a38721ae11052f
tmdgur23 <dltmdgur08@naver.com>
2026-05-05 17:46:12 +0900

    merge

--- Android-main/senior-care-app/src/App.js
+++
@@ -1,2 +0,0 @@
-import App from './remicare2/src/App';
-export default App;
--- Android-main/senior-care-app/src/remicare2/src/constants.js
+++
@@ -1,4 +0,0 @@
-// constants.js
-export const BASE_URL = window.location.hostname === "localhost" 
-  ? "http://localhost:8080" 
-  : "http://192.168.1.102:8080";
